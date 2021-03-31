3073c5a2bd42e8e6fb3c708dd6dcaec1170af66b
RusinTomasz <tomaszrusin90@gmail.com>
2021-03-31 09:04:00 +0200

    Split Footer component into FooterCircle and FooterNavigation

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,103 +1,22 @@
 import React from "react";
 import Logo from "../logo/Logo";
-import Link from "next/link";
 
 // Styles
-import styled, { css } from "styled-components";
-import { colors, fontFamilies } from "../../styles/variables";
+import styled from "styled-components";
 import FlexWrapper from "../../styles/shared/FlexWrapper";
 import Container from "../../styles/shared/Container";
 import { maxWidth, breakpoints } from "../../styles/breakpoints";
 import Image from "next/image";
 
-const flexDisplay = css`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const CircleLink = styled.a`
-  background-color: ${colors.yellow};
-  border-radius: 50%;
-  width: 150px;
-  height: 150px;
-  display: flex;
-  align-items: center;
-  padding: 1rem;
-  font-weight: 300;
-  font-size: 24px;
-  line-height: 34px;
-  font-family: ${fontFamilies.teko};
-  text-transform: uppercase;
-  ${maxWidth(breakpoints.sm)} {
-    width: 110px;
-    height: 110px;
-    font-size: 20px;
-    line-height: 28px;
-  }
-`;
-
-const CircleLinkWrap = styled.div`
-  width: 100%;
-  max-width: 80%;
-  text-align: right;
-  display: flex;
-  justify-content: flex-end;
-`;
-
-const FooterTitle = styled.h3`
-  font-size: 32px;
-  line-height: 48px;
-  font-weight: 500;
-  color: white;
-  text-align: center;
-  max-width: 80%;
-  position: relative;
-  padding-top: 4rem;
-  ${maxWidth(breakpoints.sm)} {
-    font-size: 24px;
-    line-height: 30px;
-  }
-  &::after {
-    content: "";
-    position: absolute;
-    top: 0;
-    left: 46%;
-    transform: translateX(-50%);
-    background-image: url("/img/footer/star.svg");
-    transform: rotate(-90deg);
-    background-repeat: no-repeat;
-    background-size: 3rem;
-    background-position: center;
-    width: 3rem;
-    height: 3rem;
-  }
-`;
-
-const FooterCircle = styled.div`
-  position: absolute;
-  top: -430px;
-  left: 50%;
-  transform: translateX(-50%);
-  width: 490px;
-  height: 490px;
-  padding: 6rem;
-  border-radius: 50%;
-  background-color: ${colors.blue};
-  ${flexDisplay};
-  flex-wrap: wrap;
-  z-index: 5;
-  ${maxWidth(breakpoints.sm)} {
-    padding: 2rem;
-    width: 280px;
-    height: 280px;
-    top: -340px;
-  }
-`;
+//Components
+import FooterNavigation from "./footerNav/FooterNavigation";
+import FooterCircle from "./footerCircle/FooterCircle";
 
 const FooterShape = styled.div`
   min-height: 472px;
-  ${flexDisplay};
+  display: flex;
+  align-items: center;
+  justify-content: center;
   position: relative;
 `;
 
@@ -108,29 +27,6 @@ const FooterBackground = styled.div`
   @include sm {
     height: 450px;
   }
-`;
-
-const FooterNavTitle = styled.h3`
-  font-weight: bold;
-  font-size: 18px;
-  line-height: 16px;
-  text-transform: uppercase;
-`;
-
-const FooterNav = styled.ul`
-  padding: 0;
-`;
-const FooterNavListItem = styled.li`
-  padding: 0;
-  list-style: none;
-`;
-
-const FooterLink = styled.a`
-  color: white;
-  font-weight: bold;
-  font-size: 14px;
-  line-height: 16px;
-  margin: 1rem 0;
 `;
 
 const FooterParagraph = styled.p`
@@ -166,40 +62,11 @@ function Footer() {
         />
       </FooterBackground>
       <FooterShape>
-        <FooterCircle>
-          <FooterTitle>Nie znalazłeś/łaś swojej myjni?</FooterTitle>
-          <CircleLinkWrap>
-            <Link href="/kontakt" passHref>
-              <CircleLink>+ Dodaj myjnię</CircleLink>
-            </Link>
-          </CircleLinkWrap>
-        </FooterCircle>
+        <FooterCircle />
         <FooterBottom>
           <FlexWrapper justifyContent="space-between" wrap="wrap">
             <FooterColumn>
-              <FooterNavTitle>Linki</FooterNavTitle>
-              <FooterNav>
-                <FooterNavListItem>
-                  <Link href="/kontakt" passHref>
-                    <FooterLink>Dodaj myjnię</FooterLink>
-                  </Link>
-                </FooterNavListItem>
-                <FooterNavListItem>
-                  <Link href="/kontakt" passHref>
-                    <FooterLink>Kontakt</FooterLink>
-                  </Link>
-                </FooterNavListItem>
-                <FooterNavListItem>
-                  <Link href="/policy" passHref>
-                    <FooterLink>Polityka prywatności</FooterLink>
-                  </Link>
-                </FooterNavListItem>
-                <FooterNavListItem>
-                  <Link href="/cookies" passHref>
-                    <FooterLink>Polityka cookies</FooterLink>
-                  </Link>
-                </FooterNavListItem>
-              </FooterNav>
+              <FooterNavigation />
             </FooterColumn>
             <FooterColumn>
               <Logo />
