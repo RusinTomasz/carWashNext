f9f3912adfe6584546a9d62ba24aa53c0a8aaaad
RusinTomasz <tomaszrusin90@gmail.com>
2021-04-01 09:13:59 +0200

    ActionLink styled component

---
+++ app/styles/shared/links/ActionLink.tsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import { colors } from "../../variables";
+import Fonts from "./fonts";
+
+interface ActionLinkProps {
+  backgroundColor?: "blue" | "red" | "default";
+  font?: "teko" | "exo";
+}
+
+const ActionLink = styled.a<ActionLinkProps>`
+  display: inline-block;
+  padding: 0.5rem 1.5rem 0.2rem;
+  font-size: 21px;
+  line-height: 30px;
+  text-transform: uppercase;
+  color: ${colors.white};
+  border-radius: 3px;
+  border: none;
+  cursor: pointer;
+  background-color: ${(props) =>
+    `${colors[props.backgroundColor]}` || `${colors.default}`};
+  font-family: ${(props) => Fonts[props.font] || '"Exo", sans-serif'};
+`;
+
+export default ActionLink;
---
+++ app/styles/shared/links/fonts.tsx
@@ -0,0 +1,5 @@
+const teko = '"Teko", sans-serif';
+
+const exo = '"Exo", sans-serif';
+
+export default { teko, exo };
