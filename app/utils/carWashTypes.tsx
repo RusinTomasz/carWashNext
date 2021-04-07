28209f7c1a4d7aa794233f031870ef312d5dc96c
RusinTomasz <tomaszrusin90@gmail.com>
2021-04-07 08:32:07 +0200

    getCarWashTypeIdByAlias function

--- app/utils/carWashTypes.tsx
+++ app/utils/carWashTypes.tsx
@@ -1,8 +1,17 @@
 import { CarWashTypes } from "../types/CarWash";
 
 const carWashTypes: CarWashTypes = {
-  autospa: "autospa",
-  touchless: "myjniebezdotykowe",
+  autospa: { alias: "autospa", id: 2 },
+  touchless: { alias: "myjniebezdotykowe", id: 1 },
+};
+
+export const getCarWashTypeIdByAlias = (alias: string): number => {
+  const carWashType: string[] = Object.keys(carWashTypes).filter(
+    (key) => carWashTypes[key].alias === alias
+  );
+  const carWashId: number = carWashTypes[carWashType[0]].id;
+
+  return carWashId;
 };
 
 export default carWashTypes;
