28d05116b23e8084e74e4f94bac16b0c686db8c9
RusinTomasz <tomaszrusin90@gmail.com>
2021-04-19 11:27:00 +0200

    addReview api function

---
+++ app/api/addReview.tsx
@@ -0,0 +1,44 @@
+import axios from "axios";
+
+interface addReviewArgs {
+  refId: number;
+  authorName: string;
+  message: string;
+  starsValue: number;
+}
+
+const addReview = async (review: addReviewArgs) => {
+  const { authorName, message, starsValue, refId } = review;
+
+  const reviewToAdd = {
+    authorName: authorName,
+    content: message,
+    blocked: false,
+    points: starsValue,
+    related: [
+      {
+        refId: refId,
+        ref: "car-wash",
+        field: "comments",
+      },
+    ],
+  };
+
+  try {
+    const addedReview = await axios.post(
+      `${process.env.NEXT_PUBLIC_HOST}/comments/car-wash:${refId}`,
+      reviewToAdd,
+      {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    return addedReview;
+  } catch (error) {
+    return;
+  }
+};
+
+export default addReview;
