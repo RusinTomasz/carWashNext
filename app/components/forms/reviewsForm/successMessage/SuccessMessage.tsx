82aa2fd6b8cffce49e3a188261eb11da00b4cdd8
RusinTomasz <tomaszrusin90@gmail.com>
2021-04-19 11:27:32 +0200

    SuccessMessage Component in reviewsForm Module

---
+++ app/components/forms/reviewsForm/successMessage/SuccessMessage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SuccessMessage = () => {
+  return (
+    <p>
+      Gratulacje Twoja opinia została przesłana. Opinia wymaga zatwierdzenia
+      przez administora. Powinna pojawić się w ciągu 24h.
+    </p>
+  );
+};
+
+export default SuccessMessage;
