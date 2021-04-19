3e61f0c761cf3d2ea0df169277e391649d443fe9
RusinTomasz <tomaszrusin90@gmail.com>
2021-04-19 11:27:25 +0200

    ErrorMessage Component in reviewsForm Module

---
+++ app/components/forms/reviewsForm/errorMessage/ErrorMessage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ErrorMessage = () => {
+  return (
+    <p>
+      Niestety nie udało się przesłać Twojej opinii. Spróbuj ponownie później.
+    </p>
+  );
+};
+
+export default ErrorMessage;
