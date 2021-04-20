7bfe3de15f27e0fbd50d27e85a85282ed9666ec1
RusinTomasz <tomaszrusin90@gmail.com>
2021-04-20 08:57:28 +0200

    hardcoded voivodeships data

---
+++ app/components/voivodeships/data/voivodeshipsData.tsx
@@ -0,0 +1,68 @@
+const voivodeships = [
+  {
+    slug: "dolnoslaskie",
+    name: "dolnośląskie",
+  },
+  {
+    slug: "kujawsko-pomorskie",
+    name: "kujawsko-pomorskie",
+  },
+  {
+    slug: "lodzkie",
+    name: "łódzkie",
+  },
+  {
+    slug: "lubelskie",
+    name: "lubelskie",
+  },
+  {
+    slug: "lubuskie",
+    name: "lubuskie",
+  },
+  {
+    slug: "malopolskie",
+    name: "małopolskie",
+  },
+  {
+    slug: "mazowieckie",
+    name: "mazowieckie",
+  },
+  {
+    slug: "opolskie",
+    name: "opolskie",
+  },
+  {
+    slug: "podkarpackie",
+    name: "podkarpackie",
+  },
+  {
+    slug: "podlaskie",
+    name: "podlaskie",
+  },
+  {
+    slug: "pomorskie",
+    name: "pomorskie",
+  },
+  {
+    slug: "slaskie",
+    name: "śląskie",
+  },
+  {
+    slug: "swietokrzyskie",
+    name: "świętokrzyskie",
+  },
+  {
+    slug: "warminsko-mazurskie",
+    name: "warmińsko-mazurskie",
+  },
+  {
+    slug: "wielkopolskie",
+    name: "wielkopolskie",
+  },
+  {
+    slug: "zachodniopomorskie",
+    name: "zachodniopomorskie",
+  },
+];
+
+export default voivodeships;
