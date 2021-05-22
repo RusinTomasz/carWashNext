ceb8ac172071dfd0cb07028b390883af73241a6c
RusinTomasz <tomaszrusin90@gmail.com>
2021-05-22 09:42:30 +0200

    If no reviews.length return 0

--- app/utils/countScoreFromReviews.tsx
+++ app/utils/countScoreFromReviews.tsx
@@ -2,6 +2,9 @@ import Review from "../types/Review";
 
 const countScoreFromReviews = (reviews: Review[]) => {
   let score = 0;
+  if (!(reviews.length > 0)) {
+    return 0;
+  }
   reviews.map((review) => (score += review.points));
   return score / reviews.length;
 };
