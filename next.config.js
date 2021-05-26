fdd9798de3a3cd0c56ebc8e7c2da8382eda86259
trusin <trusin@adsolutions.pl>
2021-05-26 10:52:16 +0200

    .babelrc use browserslist from package.json

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   images: {
-    domains: ["localhost", "lh5.googleusercontent.com"],
+    domains: ["localhost", "lh5.googleusercontent.com", "rankingmyjni.pl"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
 };
