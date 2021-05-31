d2c2003539fcf78ce8b0aa71dc1e137092d39059
RusinTomasz <tomaszrusin90@gmail.com>
2021-05-31 20:46:46 +0200

    remove Map from LocationMap

--- app/components/locationMap/LocationMap.tsx
+++ app/components/locationMap/LocationMap.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+// import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import styled from "styled-components";
 
 interface LocationMapProps {
@@ -7,12 +7,12 @@ interface LocationMapProps {
   popupTitle: string;
 }
 
-const Map = styled(MapContainer)`
-  height: 300px;
-  position: relative;
-  outline: none;
-  margin: 5rem 0;
-`;
+// const Map = styled(MapContainer)`
+//   height: 300px;
+//   position: relative;
+//   outline: none;
+//   margin: 5rem 0;
+// `;
 
 const LocationMap = (props: LocationMapProps) => {
   const { position, popupTitle } = props;
@@ -21,12 +21,13 @@ const LocationMap = (props: LocationMapProps) => {
     '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors';
 
   return (
-    <Map center={position} maxZoom={18} zoom={9}>
-      <TileLayer url={titleLayerUrl} attribution={attribution} />
-      <Marker key={1} position={position}>
-        <Popup>{popupTitle}</Popup>
-      </Marker>
-    </Map>
+    <div>Mapa</div>
+    // <Map center={position} maxZoom={18} zoom={9}>
+    //   <TileLayer url={titleLayerUrl} attribution={attribution} />
+    //   <Marker key={1} position={position}>
+    //     <Popup>{popupTitle}</Popup>
+    //   </Marker>
+    // </Map>
   );
 };
 
