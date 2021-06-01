d4ab278c4d95ba7bb8b27b2f77b80eb933468674
RusinTomasz <tomaszrusin90@gmail.com>
2021-06-01 18:20:59 +0200

    PrivacyAndCookiesPolicy Page

---
+++ pages/polityka-prywatnosci-i-cookies/index.tsx
@@ -0,0 +1,197 @@
+import Head from "next/head";
+import Link from "next/link";
+import React from "react";
+import BreadcrumbsComponent from "../../app/components/breadcrumbs/Breadcrumbs";
+import Container from "../../app/styles/shared/Container";
+
+const PrivacyAndCookiesPolicy = () => {
+  return (
+    <>
+      <Head>
+        <title>Polityka prywatnośći i cookies</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <BreadcrumbsComponent />
+        <Container>
+          <h1>
+            Polityka prywatności serwisu <Link href="/">rankingmyjni.pl</Link>
+          </h1>
+          <ol>
+            <li>
+              Informacje og&oacute;lne. Niniejsza Polityka Prywatności określa
+              zasady przetwarzania i ochrony danych osobowych przekazanych przez
+              Użytkownik&oacute;w w związku z korzystaniem przez nich usług
+              rankingmyjni.pl poprzez Serwis.
+            </li>
+            <li>
+              Administratorem danych osobowych zawartych w serwisie jest
+              Administracja Serwisu.
+            </li>
+            <li>
+              W trosce o bezpieczeństwo powierzonych nam danych opracowaliśmy
+              wewnętrzne procedury i zalecenia, kt&oacute;re mają zapobiec
+              udostępnieniu danych osobom nieupoważnionym. Kontrolujemy ich
+              wykonywanie i stale sprawdzamy ich zgodność z odpowiednimi aktami
+              prawnymi - ustawą o ochronie danych osobowych, ustawą o
+              świadczeniu usług drogą elektroniczną, a także wszelkiego rodzaju
+              aktach wykonawczych i aktach prawa wsp&oacute;lnotowego.
+            </li>
+            <li>
+              Dane Osobowe przetwarzane są na podstawie zgody wyrażanej przez
+              Użytkownika oraz w przypadkach, w kt&oacute;rych przepisy prawa
+              upoważniają Administratora do przetwarzania danych osobowych na
+              podstawie przepis&oacute;w prawa lub w celu realizacji usługi.
+            </li>
+            <li>
+              Serwis realizuje funkcje pozyskiwania informacji o użytkownikach i
+              ich zachowaniach w następujący spos&oacute;b:
+              <ul>
+                <li>
+                  a. poprzez dobrowolnie wprowadzone w formularzach informacje
+                </li>
+                <li>
+                  b. poprzez gromadzenie plik&oacute;w &ldquo;cookies&rdquo;
+                  [patrz polityka plik&oacute;w &ldquo;cookies&rdquo;].
+                </li>
+              </ul>
+            </li>
+            <li>
+              Serwis zbiera informacje dobrowolnie podane przez użytkownika.
+            </li>
+            <li>
+              Dane podane w formularzu są przetwarzane w celu wynikającym z
+              funkcji konkretnego formularza np. w celu dokonania procesu
+              obsługi kontaktu informacyjnego
+            </li>
+            <li>
+              Dane Osobowe mogą zostać przekazane innym podmiotom, na podstawie
+              zgody wyrażanej przez Użytkownika, w celu realizacji usługi.
+            </li>
+            <li>
+              Pozostałe dane osobowe pozostawione w serwisie nie zostaną
+              sprzedane ani udostępnione innym podmiotom.
+            </li>
+            <li>
+              Do danych zawartych w formularzu przysługuje wgląd osobie
+              fizycznej, kt&oacute;ra je tam umieściła. Osoba ta ma
+              r&oacute;wnież praw do modyfikacji i zaprzestania przetwarzania
+              swoich danych w dowolnym momencie.
+            </li>
+            <li>
+              Zastrzegamy sobie prawo do zmiany w polityce ochrony prywatności
+              serwisu, na kt&oacute;re może wpłynąć rozw&oacute;j technologii
+              internetowej, ewentualne zmiany prawa w zakresie ochrony danych
+              osobowych oraz rozw&oacute;j naszego serwisu internetowego. O
+              wszelkich zmianach będziemy informować w spos&oacute;b widoczny i
+              zrozumiały.
+            </li>
+            <li>
+              W Serwisie mogą pojawiać się linki do innych stron internetowych.
+              Takie strony internetowe działają niezależnie od Serwisu i nie są
+              w żaden spos&oacute;b nadzorowane przez serwis rankingmyjni.pl .
+              Strony te mogą posiadać własne polityki dotyczące prywatności oraz
+              regulaminy, z kt&oacute;rymi zalecamy się zapoznać. W razie
+              wątpliwości co kt&oacute;regokolwiek z zapis&oacute;w niniejszej
+              polityki prywatności jesteśmy do dyspozycji - nasze dane znaleźć
+              można w zakładce &ndash; KONTAKT.
+            </li>
+          </ol>
+          <h2>
+            Polityka plików “cookies” serwisu{" "}
+            <Link href="/">rankingmyjni.pl</Link>
+          </h2>
+          <ol>
+            <li>
+              Poprzez pliki &ldquo;cookies&rdquo; należy rozumieć dane
+              informatyczne przechowywane w urządzeniach końcowych
+              użytkownik&oacute;w, przeznaczone do korzystania ze stron
+              internetowych. W szczeg&oacute;lności są to pliki tekstowe,
+              zawierające nazwę strony internetowej, z kt&oacute;rej pochodzą,
+              czas przechowywania ich na urządzeniu końcowym oraz unikalny
+              numer.
+            </li>
+            <li>
+              Serwis nie zbiera w spos&oacute;b automatyczny żadnych informacji,
+              z wyjątkiem informacji zawartych w plikach cookies.
+            </li>
+            <li>
+              Pliki cookies przeznaczone są do korzystania ze stron serwisu.
+              Operator wykorzystuje te pliki do:
+              <ul>
+                <li>
+                  możliwości logowania i utrzymywania sesji użytkownika na
+                  każdej kolejnej stronie serwisu
+                </li>
+                <li>
+                  dopasowania zawartości strony internetowej do indywidualnych
+                  preferencji użytkownika, przede wszystkim pliki te rozpoznają
+                  jego urządzenie, aby zgodnie z jego preferencjami wyświetlić
+                  stronę
+                </li>
+                <li>
+                  do tworzenia anonimowych statystyk z wyłączeniem możliwości
+                  identyfikacji użytkownika.
+                </li>
+              </ul>
+            </li>
+            <li>
+              Pliki cookies wykorzystywane przez partner&oacute;w operatora
+              strony internetowej, w tym w szczeg&oacute;lności
+              użytkownik&oacute;w strony internetowej, podlegają ich własnej
+              polityce prywatności.
+            </li>
+            <li>
+              W trosce o bezpieczeństwo powierzonych nam danych opracowaliśmy
+              wewnętrzne procedury i zalecenia, kt&oacute;re mają zapobiec
+              udostępnieniu danych osobom nieupoważnionym. Kontrolujemy ich
+              wykonywanie i stale sprawdzamy ich zgodność z odpowiednimi aktami
+              prawnymi - ustawą o ochronie danych osobowych, ustawą o
+              świadczeniu usług drogą elektroniczną, a także wszelkiego rodzaju
+              aktach wykonawczych i aktach prawa wsp&oacute;lnotowego
+            </li>
+            <li>
+              Standardowo oprogramowanie służące do przeglądania stron
+              internetowych domyślnie dopuszcza umieszczanie plik&oacute;w
+              cookies na urządzeniu końcowym Użytkownika. Ustawienia te mogą
+              zostać zmienione przez Użytkownika w taki spos&oacute;b, aby
+              blokować automatyczną obsługę &ldquo;cookies&rdquo; w ustawieniach
+              przeglądarki internetowej bądź informować o ich każdorazowym
+              przesłaniu na urządzenia użytkownika.
+            </li>
+            <li>
+              Użytkownicy Serwisu mogą dokonać w każdym czasie zmiany ustawień
+              dotyczących plik&oacute;w cookies. Szczeg&oacute;łowe informacje o
+              możliwości i sposobach obsługi plik&oacute;w cookies dostępne są w
+              ustawieniach oprogramowania (przeglądarki internetowej).
+              Przykładowe opcje edytowania w popularnych przeglądarkach:
+              <ul>
+                <li>
+                  ○ Mozilla Firefox: www.support.mozilla.org/pl/kb/ciasteczka
+                </li>
+                <li>
+                  ○ Internet Explorer: www.support.microsoft.com/kb/278835/pl
+                </li>
+                <li>
+                  ○ Google Chrome:
+                  www.support.google.com/chrome/bin/answer.py?hl=pl&amp;answer=95647
+                </li>
+                <li>
+                  ○ Safari:
+                  www.safari.helpmax.net/pl/oszczedzanie-czasu/blokowanie-zawartosci/
+                </li>
+              </ul>
+            </li>
+            <li>
+              Operator Serwisu informuje, że zmiany ustawień w przeglądarce
+              internetowej użytkownika mogą uniemożliwić poprawne działanie
+              Stron Internetowych.
+            </li>
+          </ol>
+        </Container>
+      </main>
+    </>
+  );
+};
+
+export default PrivacyAndCookiesPolicy;
