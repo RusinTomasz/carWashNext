d618133f032800115a71070d7a73a9ef4a38ecf2
RusinTomasz <tomaszrusin90@gmail.com>
2021-06-12 09:28:43 +0200

    disable webpack5

--- next.config.js
+++ next.config.js
@@ -11,9 +11,9 @@ const withTM = require("next-transpile-modules")([
 ]);
 
 module.exports = withTM({
-  future: {
-    webpack5: true,
-  },
+  // future: {
+  //   webpack5: true,
+  // },
   images: {
     domains: ["localhost", "lh5.googleusercontent.com", "rankingmyjni.pl"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
