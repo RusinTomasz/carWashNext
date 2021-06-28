bcb08aafe8ebe546569578eb2c6078031d01dd93
RusinTomasz <tomaszrusin90@gmail.com>
2021-06-28 19:08:43 +0200

    Temporary [type] page

--- pages/[type]/index.tsx
+++ pages/[type]/index.tsx
@@ -1,15 +1,54 @@
 import Head from "next/head";
+import voivodeships from "../../app/components/voivodeships/data/voivodeshipsData";
+import VoivodeshipsList from "../../app/components/voivodeships/voivodeshipsList/VoivodeshipsList";
+import Container from "../../app/styles/shared/Container";
+import FlexWrapper from "../../app/styles/shared/FlexWrapper";
 
-export default function CarWashType() {
+interface TypeParams {
+  type: string;
+}
+
+interface CarWashTypeProps {
+  type: string;
+  canonicalUrl: string;
+}
+
+export default function CarWashType(props: CarWashTypeProps) {
+  const { type, canonicalUrl } = props;
+
   return (
     <>
       <Head>
-        <title>Ranking Myjni</title>
+        <title>{`${
+          type === "myjniebezdotykowe" ? "Myjnie bezdotykowe" : "Auto detailing"
+        } | rankingmyjni.pl`}</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="canonical" href={canonicalUrl} />
       </Head>
       <main>
-        <h1>Temporary page</h1>
+        <Container>
+          <FlexWrapper justifyContent="center">
+            {/* Needs to be done more dynamicaly #review */}
+            <h1>{`${
+              type === "myjniebezdotykowe"
+                ? "Myjnie bezdotykowe"
+                : "Auto detailing"
+            } - Województwa`}</h1>
+          </FlexWrapper>
+          <VoivodeshipsList type={type} voivodeships={voivodeships} />
+        </Container>
       </main>
     </>
   );
 }
+
+export const getServerSideProps = async ({ req, query }) => {
+  const { type }: TypeParams = query;
+
+  return {
+    props: {
+      type,
+      canonicalUrl: `https://${req.headers.host}${req.url}`,
+    },
+  };
+};
