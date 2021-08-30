e1a880d1e25a948742f8e936979dc0971f004500
Tomasz <tomaszrusin90@gmail.com>
2021-08-30 22:25:12 +0200

    Remove useForm hook and all logic for LoginForm

--- app/components/forms/loginForm/index.tsx
+++ app/components/forms/loginForm/index.tsx
@@ -1,22 +1,10 @@
-import React, {SyntheticEvent, useState} from 'react';
-import FormStatusMessage from "../reviewsForm/formStatusMessage/FormStatusMessage";
-import {
-    errorEmailMessage,
-    errorEmailTitle,
-    successEmailMessage,
-    successEmailTitle
-} from "../../../constants/formsMessages/FormMessages";
+import React, {SyntheticEvent } from 'react';
 import LoginForm from "./LoginForm";
-import ReCAPTCHA from "react-google-recaptcha";
-import InputEvent from "../../../types/InputEvent";
-import useForm from "../../../hooks/useForm";
-
 
 export interface LoginFormValues {
     email: string;
     password: string;
 }
-
 
 const LoginFormContainer = () => {
 
@@ -24,66 +12,30 @@ const LoginFormContainer = () => {
         email: "",
         password: "",
     };
-    const [state, setState] = useState(formValues);
-
-    const handleState = (state) => {
-        setState(() => state);
-    };
-
-    const {
-        isError,
-        isSuccess,
-        isLoading,
-        isCaptchaVerified,
-        recaptchaRef,
-        resetForm,
-        setLoadingStatus,
-        resetFormStatus,
-        onCaptchaChange,
-        handleChangeFormValues
-    } = useForm(state, handleState, formValues);
-
 
     const handleSubmit = async (evt: SyntheticEvent) => {
         evt.preventDefault();
-        resetFormStatus();
-        setLoadingStatus(true);
-
-        // await sendEmail();
-
         console.log("await for login");
-
-        setLoadingStatus(false);
-        resetForm();
     };
-
 
     return (
         <>
-            <LoginForm
-                recaptchaRef={recaptchaRef}
-                onCaptchaChange={onCaptchaChange}
-                isCaptchaVerified={isCaptchaVerified}
-                handleChangeFormValues={handleChangeFormValues}
-                handleSubmit={handleSubmit}
-                state={state}
-                isLoading={isLoading}
-            />
+            <LoginForm />
 
-            {isSuccess && (
-                <FormStatusMessage
-                    title={successEmailTitle}
-                    message={successEmailMessage}
-                    type="success"
-                />
-            )}
-            {isError && (
-                <FormStatusMessage
-                    title={errorEmailTitle}
-                    message={errorEmailMessage}
-                    type="error"
-                />
-            )}
+            {/*{isSuccess && (*/}
+            {/*    <FormStatusMessage*/}
+            {/*        title={successEmailTitle}*/}
+            {/*        message={successEmailMessage}*/}
+            {/*        type="success"*/}
+            {/*    />*/}
+            {/*)}*/}
+            {/*{isError && (*/}
+            {/*    <FormStatusMessage*/}
+            {/*        title={errorEmailTitle}*/}
+            {/*        message={errorEmailMessage}*/}
+            {/*        type="error"*/}
+            {/*    />*/}
+            {/*)}*/}
         </>
     );
 };
--- app/types/Captcha.tsx
+++ app/types/Captcha.tsx
@@ -1,4 +1,5 @@
 import ReCAPTCHA from "react-google-recaptcha";
+import React from "react";
 
 export default interface CaptchaProps {
   recaptchaRef: React.RefObject<ReCAPTCHA>;
