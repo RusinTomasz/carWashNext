5c2dabb21b156fa097c6ed18b7fb25817f6b68bc
RusinTomasz <tomaszrusin90@gmail.com>
2021-09-09 10:49:08 +0200

    Login Page init

--- pages/auth/login/index.tsx
+++ pages/auth/login/index.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Container from "../../../app/styles/shared/Container";
 import LoginForm from "../../../app/components/forms/loginForm";
+import {getJwt} from "../../../app/utils/authentication";
+
 
 const Login = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const jwt = getJwt();
+        if (jwt) setIsLoggedIn(true);
+    }, []);
+
+
     return (
         <Container>
-           <LoginForm />
+            {isLoggedIn ? <div> Jesteś już zalogowany </div> : <LoginForm/>}
         </Container>
     );
 };
